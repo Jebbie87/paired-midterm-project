232a9483ae924a5bb3aa256926939945fee335aa
ak-11 <awkim.11@gmail.com>
2016-11-24 21:53:53 +0000

    Working code for form input being pushed to database through knex.

--- routes/events.js
+++ routes/events.js
@@ -9,31 +9,54 @@ module.exports = (knex) => {
     res.render("./events/new")
   })
 
-  router.post("/", (req, res) => {
+  router.post("/new", (req, res) => {
     const title = req.body.title;
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
     const email = req.body.email;
     const description = req.body.description;
     const date = req.body.date;
+
     knex("events")
       .insert([{title: title, date: date, description: description}])
+      .then(() => {
+        knex("attendees")
+          .insert([{first_name: firstName, last_name: lastName, email: email}])
+          .then(() => {
+            console.log("Success brah!");
+            // res.redirect("/uniqueURL")
+          })
+          .catch((err) => {
+            console.log(err);
+          })
+          // .finally(() => {
+          //   knex.destroy();
+          // })
+      })
+      .then(() => {
+        console.log("Success brah!");
+        // res.redirect("/uniqueURL")
+      })
       .catch((err) => {
         console.log(err);
       })
-      .finally(() => {
-        knex.destroy();
-      })
-
-    knex("attendees")
-      .insert([{first_name: firstName, last_name: lastName, email: email}])
-      .catch((err) => {
-        console.log(err);
-      })
-      .finally(() => {
-        knex.destroy();
-      })
+      // .finally(() => {
+      //   knex.destroy();
+      // })
+      res.redirect("/");
   })
-
   return router;
 }
+  //   knex("attendees")
+  //     .insert([{first_name: firstName, last_name: lastName, email: email}])
+  //     .then(() => {
+  //       console.log("Success brah!");
+  //       // res.redirect("/uniqueURL")
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     })
+  //     .finally(() => {
+  //       knex.destroy();
+  //     })
+  // })
