a472c05924d166807a8da969850aac288195ad01
Jeffrey Chang <jeffreycj.chang@gmail.com>
2016-11-25 02:22:16 +0000

    Finished chaining knex insert

--- routes/events-response.js
+++ routes/events-response.js
@@ -18,27 +18,35 @@ const counter = {
   });
 
   router.post('/:uniqueurl', (req, res) => {
-    // const personGoing = Number(req.body.going);
     const time1 = Number(req.body.going1);
-    // console.log(req.body)
     const time2 = Number(req.body.going2);
     const time3 = Number(req.body.going3);
+    const userFirstName = req.body['first-name'];
+    const userLastName = req.body['last-name'];
+    const userEmail = req.body['user-email'];
 
     counter.time1 += time1;
     counter.time2 += time2;
     counter.time3 += time3;
 
+    knex('attendees')
+      .insert([ {first_name: userFirstName, last_name: userLastName, email: userEmail} ])
+      .returning('id')
+      .then(function(attendeesId) {
+        let id = Number(attendeesId[0]);
 
-
-    // console.log("events response body:", req.body.going)
-    // knex('response')
-    //   .insert( {response: personGoing} )
-    //   .then(function(results) {
-    //     console.log(results);
-    //   })
-    //   .catch(function(err) {
-    //     console.log(err);
-    //   })
+        knex('response')
+          .insert([ {response: time1, attendees_id: id}, {response: time2, attendees_id: id}, {response: time3, attendees_id: id} ])
+          .then(function(results) {
+            console.log(results);
+          })
+          .catch(function(err) {
+            console.log(err);
+          })
+      })
+      .catch(function(err) {
+        console.log(err)
+      })
 
     // knex('event_times')
     //   .insert( [{times: time1}, {times: time2}, {times: time3}] )
